aa36ee9679f04424626c94ec5b84f26cdf1f9eb4
Ken Swenson <flat@imo.uto.moe>
2018-04-13 20:26:51 -0400

    Updated build w/ new outputs

--- bin/fixmonitors.sh
+++ bin/fixmonitors.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xrandr --output HDMI-0 --primary --auto --left-of DVI-I-1
-xrandr --output DVI-I-1 --auto --right-of HDMI-0
+xrandr --output DVI-I-1 --primary --auto --left-of DP-0
+xrandr --output DP-0 --auto --right-of DVI-I-1
 gsettings set org.gnome.desktop.background picture-uri file:///home/flat/Pictures/Wallpapers/span_wall.png
 gsettings set org.gnome.desktop.background picture-options spanned
