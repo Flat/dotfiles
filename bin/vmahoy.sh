7097b8c25e8b5607bb9e18f4445f52b7d4dc59dc
Ken Swenson <flat@esoteric.moe>
2018-09-08 12:00:44 -0400

    Switch back to using other vm

--- bin/vmahoy.sh
+++ bin/vmahoy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo virsh start win10-2
+sudo virsh start win10
 xrandr --output DVI-I-1 --off
 gsettings set org.gnome.desktop.background picture-uri file:///home/flat/Pictures/Wallpapers/rightwall.png
 gsettings set org.gnome.desktop.background picture-options scaled
