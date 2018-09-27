2cabbe4ac2f2e2d00c6a045c19005f897facf8bd
Ken Swenson <flat@esoteric.moe>
2018-09-26 20:48:18 -0400

    Fix scripts

--- bin/fixmonitors.sh
+++ bin/fixmonitors.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-xrandr --output DVI-I-1 --primary --auto --left-of DP-0
-xrandr --output DP-0 --auto --right-of DVI-I-1
-gsettings set org.gnome.desktop.background picture-uri file:///home/flat/Pictures/Wallpapers/span_wall.png
-gsettings set org.gnome.desktop.background picture-options spanned
+xrandr --output DVI-I-1 --primary --auto --left-of DP-1
+xrandr --output DP-1 --auto --right-of DVI-I-1
--- bin/vmahoy.sh
+++ bin/vmahoy.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 sudo virsh start win10
 xrandr --output DVI-I-1 --off
-gsettings set org.gnome.desktop.background picture-uri file:///home/flat/Pictures/Wallpapers/rightwall.png
-gsettings set org.gnome.desktop.background picture-options scaled
